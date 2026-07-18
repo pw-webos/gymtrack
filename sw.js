664d2dbd157d7ffad2fe2a97c133f483e4e7ed49
pw-webos <paragwatts@gmail.com>
2026-07-18 19:44:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+var CACHE = "gymtrack-v1";
+var ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+});
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      return res;
+    }).catch(function(){ return caches.match(e.request); })
+  );
+});
